13f8426aeb0295f662a54ca468da698cdda241d2
one99 <goodloveone1@gmail.com>
2018-06-23 15:49:22 +0700

    edit

--- module/personnel/editsubject.php
+++ module/personnel/editsubject.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php
-    $result=mysqli_query($con,"SELECT branch_id,branch_name FROM branch WHERE branch_id='$_POST[branch_id]'") or die ("mysql error=>>".mysql_error($con));
+    $result=mysqli_query($con,"SELECT branch_id,branch_name FROM branch WHERE branch_id='$_POST[branch_ID]'") or die ("mysql error=>>".mysql_error($con));
     list($branch_id,$branch_name)=mysqli_fetch_row($result);
 
 ?>
@@ -19,9 +19,33 @@
         mysqli_free_result($result);
         mysqli_close($con);
     ?>
-<p>
-    <input type="submit" value="บันทึก"> 
-    <input type="reset" value="ยกเลิก">
-</p>
+<div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+                    <button type="button" class="btn btn-primary" id="updatesu">บันทึก</button>
+  </div>
 
 </form>
+
+<script type="text/javascript">
+
+$("#updatesu").click(function(event) {
+    var r = confirm("Press a button!");
+    if (r == true) {
+        $.post( "module/personnel/#.php", $( "#foreditbrc" ).serialize()).done(function(data,txtstuta){
+             // alert(data);
+         });
+        $('#editsub').modal("hide");
+
+        $('#editsub').on('hidden.bs.modal', function (e) {
+
+            var module1 = sessionStorage.getItem("module1");
+            var action = sessionStorage.getItem("action");
+           loadmain(module1,action);
+        })
+       
+        
+    } 
+
+   
+});
+</script>
