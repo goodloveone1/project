d49b74066f56cf05852a8fd5df569a1fc61a3af2
beer1973 <pop197317@gmail.com>
2018-06-27 14:03:35 +0700

    add

--- module/personnel/edituser.php
+++ module/personnel/edituser.php
@@ -3,9 +3,10 @@
 		<h2> แก้ไขบุคลากร </h2>
 		<button type="button" class="btn re" data-modules="personnel" data-action="edituser"> REFRE </button>
 		<button type="button" class="btn re" data-modules="personnel" data-action="mangauser"> ย้อนกลับ </button>
+
 	</div>
 </div>
-<form method="" enctype="multipart/form-data">
+<form method="" enctype="multipart/form-data" id="edituser">
 	<div class="row mt-2">
 		<div class="col-md-3">
 			<div class="card">
@@ -21,25 +22,25 @@
 			<div class="form-group row">
 				<label for="staticEmail" class="col-sm-2 col-form-label">คำนำหน้า</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="staticEmail" placeholder="คำนำหน้า">
+					<input type="text" class="form-control" id="staticEmail" placeholder="คำนำหน้า" name="titlename">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">ชื่อ</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="inputPassword" placeholder="ชื่อ">
+					<input type="text" class="form-control" id="inputPassword" placeholder="ชื่อ"  name="name">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">สกุล</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="inputPassword" placeholder="สกุล">
+					<input type="text" class="form-control" id="inputPassword" placeholder="สกุล"  name="lname">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">ตำแหน่ง</label>
 				<div class="col-sm-10">
-					<select class="form-control" id="exampleFormControlSelect1">
+					<select class="form-control" id="exampleFormControlSelect1" name="pos">
 						<option>อาจารย์</option>
 						<option>2</option>
 						<option>3</option>
@@ -51,7 +52,7 @@
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">ตำแหน่งวิชาการ</label>
 				<div class="col-sm-10">
-					<select class="form-control" id="exampleFormControlSelect1">
+					<select class="form-control" id="exampleFormControlSelect1" name="ap">
 						<option>ข้าราชการ</option>
 						<option>2</option>
 						<option>3</option>
@@ -63,7 +64,7 @@
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">สาขา</label>
 				<div class="col-sm-4">
-					<select class="form-control" id="exampleFormControlSelect1">
+					<select class="form-control" id="exampleFormControlSelect1" name="suj">
 						<option>บริหารธุรกิจ</option>
 						<option>2</option>
 						<option>3</option>
@@ -73,7 +74,7 @@
 				</div>
 				<label for="inputPassword" class="col-sm-2 col-form-label">สาขาวิชา</label>
 				<div class="col-sm-4">
-					<select class="form-control" id="exampleFormControlSelect1">
+					<select class="form-control" id="exampleFormControlSelect1" name="brn">
 						<option>สารสนเทศทางคอมพิวเตอร์</option>
 						<option>2</option>
 						<option>3</option>
@@ -85,18 +86,15 @@
 			<div class="form-group row">
 				<label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
 				<div class="col-sm-4">
-					<input type="text" class="form-control" id="staticEmail" placeholder="Username">
+					<input type="text" class="form-control" id="staticEmail" placeholder="Username" name="uname">
 				</div>
 				<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
 				<div class="col-sm-4">
-					<input type="text" class="form-control" id="staticEmail" placeholder="Password">
+					<input type="Password" class="form-control" id="staticEmail" placeholder="Password" name="passwd">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-12">
-			
-		
-	
+		<div class="col-md-12"> 		<!-- >ปริญญาตรี -->
 			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
 				<div class="col-sm-12" >
 					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาตรี</label>
@@ -109,17 +107,63 @@
 							<button type="button" class="btn btn-secondary adddegree1 btn-block">เพิ่ม</button>
 						</div>
 						<div class="col-md">
-							<button type="button" class="btn btn-secondary deldegree1 btn-block">CLEAR</button>
+							<button type="button" class="btn btn-secondary cleardegree1 btn-block">CLEAR</button>
 						</div>
 
-						<div class="loaddegree col-md-12"></div>
+						<ul class="loaddegree col-12  list-group"></ul>
 						
 					</div>
 				</span>
 			</div>
-		</div>
-	</div>
-	
+		</div> <!-- > END ปริญญาตรี -->
+
+		<div class="col-md-12"> 		<!-- >ปริญญาโท -->
+			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
+				<div class="col-sm-12" >
+					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาโท</label>
+				</div>
+				
+				<span class="col-sm-12">
+					<div class='row'>
+						
+						<div class="col-md">
+							<button type="button" class="btn btn-secondary adddegree2 btn-block">เพิ่ม</button>
+						</div>
+						<div class="col-md">
+							<button type="button" class="btn btn-secondary cleardegree2 btn-block">CLEAR</button>
+						</div>
+
+						<ul class="loaddegree2 col-12  list-group"></ul>
+						
+					</div>
+				</span>
+			</div>
+		</div> <!-- > END ปริญญาโท -->
+
+		<div class="col-md-12"> 		<!-- >ปริญญาเอก -->
+			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
+				<div class="col-sm-12" >
+					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาเอก</label>
+				</div>
+				
+				<span class="col-sm-12">
+					<div class='row'>
+						
+						<div class="col-md">
+							<button type="button" class="btn btn-secondary adddegree3 btn-block">เพิ่ม</button>
+						</div>
+						<div class="col-md">
+							<button type="button" class="btn btn-secondary cleardegree3 btn-block">CLEAR</button>
+						</div>
+
+						<ul class="loaddegree3 col-12  list-group"></ul>
+						
+					</div>
+				</span>
+			</div>		
+		</div> <!-- > END ปริญญาโท -->
+		<button type="button" class="btn updateuser" data-modules="personnel" data-action="updateuser"> ADD </button>
+	</div>	
 </form>
 <script type="text/javascript">
 
@@ -131,76 +175,167 @@
 				loadmain(module1,action);
 			});
 
-			$(window).on('load', function(){
-   				
-				sessionStorage.removeItem("count");
-								
+		
+
+// ปริญญาตรี 
+
+			$("button.cleardegree1").on("click",function(e) {
+				e.preventDefault();
+			    var r = confirm("คณต้องการล้างใช่ไหม?");
+			    if(r == true){
+					$('.loaddegree').html("");
+				}
 			});
 
-			$("button.deldegree1").on("click",function(event) {
-				sessionStorage.removeItem("count");
-				var count = 0;
-				degreeload1(count);
-			});
-
-
-			/* reload ปริญญาตรี*/
-
-			$("button.adddegree1").on("click",function(event) {
-				
-
-				var count = sessionStorage.getItem("count")
-
-				alert(count);
-				degreeload1(count);
-				
-
-				
-			});
-
-				function degreeload1(count){
-
-				if(count == null){
-					count=1;	
+			$('.loaddegree').on('click', '.deldegree1', function(e) {
+			    e.preventDefault();
+			    var r = confirm("คณต้องการลบใช่ไหม?");
+            	if (r == true) {
+			    	$(this).parent().remove();
 				}
 
+			});
 
 
-				var text =[];
-				var i;
+			$("button.adddegree1").on("click",function(e) {
+  				e.preventDefault();
+			    degreeload1()
+			});
+
+			function degreeload1(){
+
+				var count =$('.loaddegree li').length
+
+					if(count < 10 && count >= 0){
+						var text = "<li class='list-group-item'>"
+						+ "<input type='text' class='form-control' id='staticEmail' placeholder='คำนำหน้า' name='a1[]'> "
+						+ "<input type='text'class='form-control' id='staticEmail' placeholder='จบที่'  name='a2[]'> "
+						+ "<button type='button' class='btn btn-danger btn-block deldegree1 '>ลบ</button>"
+						+ "</li>"
 				
-				if(count != undefined){
-					if(count != 0){
-						for(i=0;i < count;i++){
-							text[i] = "<div class='row m-1 text11'><div class='col-sm'><input type='text' class='form-control' id='staticEmail' placeholder='คำนำหน้า'></div><div class='col-sm'><input type='text'class='form-control' id='staticEmail' placeholder='จบที่'></div><div class='col-sm-2'><button type='button' class='btn btn-danger btn-block deldegree1' data-nums="+i+">ลบ</button></div></div>";
-						}
+						 $(".loaddegree").append(text);
+					}else{
+						alert("ไม่สามารเพื่มได้แล้ว");
+					}		
 
 					}
 
-					count++;
-					sessionStorage.setItem("count",count);
-					sessionStorage.setItem("text1[]",text);
-																	
-					// $(".loaddegree").html(text);
+// END ปริญญาตรี 
 
-						$.ajax({
-							  type: "POST",
-							  url: "module/personnel/loaddata.php",
-							  data : {textdata:text},
-							  statusCode: {
-								    404: function() {
-								      alert( "page not found" );
-								    }
-								  }
-							}).done(function(data) {
-							  $(".loaddegree").html(data);	
-							});
+//  ปริญญาโท
 
-							
+			$("button.cleardegree2").on("click",function(e) {
+				e.preventDefault();
+			    var r = confirm("คณต้องการล้างใช่ไหม?");
+			    if(r == true){
+					$('.loaddegree2').html("");
+				}
+			});
+
+			$("button.adddegree2").on("click",function(e) {
+  				e.preventDefault();
+			    degreeload2()
+			});
+
+			$('.loaddegree2').on('click', '.deldegree2', function(e) {
+			    e.preventDefault();
+			    var r = confirm("คณต้องการลบใช่ไหม?");
+            	if (r == true) {
+			    	$(this).parent().remove();
+				}
+
+			});
+
+
+				function degreeload2(){
+
+					var count =$('.loaddegree2 li').length
+
+					if(count < 10 && count >= 0){
+						var text = "<li class='list-group-item'>"
+						+ "<input type='text' class='form-control' id='staticEmail' placeholder='คำนำหน้า' name='a1[]'> "
+						+ "<input type='text'class='form-control' id='staticEmail' placeholder='จบที่'  name='a2[]'> "
+						+ "<button type='button' class='btn btn-danger btn-block deldegree2 '>ลบ</button>"
+						+ "</li>"
+				
+						 $(".loaddegree2").append(text);
+					}else{
+						alert("ไม่สามารเพื่มได้แล้ว");
+					}		
 
 					}
+
+// END ปริญญาโท
+
+// ปริญญาเอก 
+
+			$("button.cleardegree3").on("click",function(e) {
+				e.preventDefault();
+			    var r = confirm("คณต้องการล้างใช่ไหม?");
+			    if(r == true){
+					$('.loaddegree3').html("");
+				}
+			});			
+
+			$("button.adddegree3").on("click",function(e) {
+  				e.preventDefault();
+			    degreeload3()
+			});
+
+			$('.loaddegree3').on('click', '.deldegree3', function(e) {
+			    e.preventDefault();
+			    var r = confirm("คณต้องการลบใช่ไหม?");
+            	if (r == true) {
+			    	$(this).parent().remove();
+				}
+
+			});
+
+
+				function degreeload3(){
+
+					var count =$('.loaddegree3 li').length
+
+					if(count < 10 && count >= 0){
+						var text = "<li class='list-group-item'>"
+						+ "<input type='text' class='form-control' id='staticEmail' placeholder='คำนำหน้า' name='a1[]'> "
+						+ "<input type='text'class='form-control' id='staticEmail' placeholder='จบที่'  name='a2[]'> "
+						+ "<button type='button' class='btn btn-danger btn-block deldegree3 '>ลบ</button>"
+						+ "</li>"
+				
+						 $(".loaddegree3").append(text);
+					}else{
+						alert("ไม่สามารเพื่มได้แล้ว");
+					}		
+
+					}
+
+// END ปริญญาเอก
+
+			
+
+			
+
+		
+
+
+
 					
-				}
+			$(".updateuser").click(function(){
+
+				 $.post( "module/personnel/updateuser.php", $( "#edituser" ).serialize()).done(function(data,txtstuta){
+            	 	alert(data);
+		         });
+
+		        // $('#editsub').modal("hide");
+
+		        // $('#editsub').on('hidden.bs.modal', function (e) {
+
+		        //     var module1 = sessionStorage.getItem("module1");
+		        //     var action = sessionStorage.getItem("action");
+		        //    loadmain(module1,action);
+		        // })
+			})	
 
 				
 				
