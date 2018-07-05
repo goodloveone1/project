595921808fe4c7213b1e55cf204f84cf6af62346
one99 <goodloveone1@gmail.com>
2018-07-05 18:03:39 +0700

    ew

--- module/personnel/updateuser.php
+++ module/personnel/updateuser.php
@@ -1,20 +1,21 @@
 <?php
 include("../../function/db_function.php");
 $con=connect_db();
-echo $_FILES['userphoto']['name'];
 
-if (empty($_FILES['userphoto']['name'])){
-	$user_photo="";
-	$updete_photo="";
-	}
-	else{
-   $sum_name=date("dmyhis")."abcdefghirstuvwxyz";//เวลา+ตัวอักษร
-   $char=substr(str_shuffle($sum_name),0,10);	//ตัดให้เหลือ 10 ตัว
-   $user_photo=$char."_".$_FILES['userphoto']['name'];//ชื่อไฟล์
-   $user_phototemp=$_FILES['userphoto']['tmp_name'];
-   copy($user_phototemp,"../../img/$user_photo");
-   $updete_photo=",user_photo='$user_photo'";
-	}
+
+echo "File >>",$_FILES['pic_u']['name'];
+if (empty($_FILES['pic_u']['name'])){
+    $pic_user="";
+     $updete_photo="";
+     }
+     else{
+    $sum_name=date("dmyhis")."abcdefghirstuvwxyz";//เวลา+ตัวอักษร
+    $char=substr(str_shuffle($sum_name),0,10);	//ตัดให้เหลือ 10 ตัว
+   $pic_user=$char."_".$_FILES['pic_u']['name'];//ชื่อไฟล์
+   $pic_usertemp=$_FILES['pic_u']['tmp_name'];
+    copy($pic_usertemp,"../../img/$pic_user");
+    $updete_photo=",gen_pict='$pic_user'";
+     }
 
 echo $_POST['gen_id'],"/";
 echo $_POST['titlename'],"/";
@@ -31,12 +32,12 @@ echo $_POST['level_id'],"/";
 echo $_POST['uname'],"/";
 echo $_POST['passwd'],"/";
 echo $_POST['permiss'],"/";
-
+echo "upName =", $updete_photo;
 
 $update="UPDATE general SET gen_id='',gen_user=' $_POST[uname]',gen_pass='$_POST[passwd]',branch_id='$_POST[brn]',subject_id='$_POST[suj]',gen_code=' $_POST[codeid]',gen_prefix=' $_POST[titlename]',gen_fname='$_POST[name]',gen_lname='$_POST[lname]',
 gen_salary=' $_POST[salary]',gen_acadeic='$_POST[ap]',level_id='$_POST[level_id]',gen_startdate='$_POST[gen_startdate]',permiss_id='echo $_POST[permiss]',gen_pos='echo $_POST[pos]',$updete_photo WHERE  gen_id='$_POST[gen_id]'";
 
-echo $update;
+//echo $update;
 //mysqli_query($con,$update)or die("SQL Error=>".mysqli_error($con));
 
 mysqli_close($con);
