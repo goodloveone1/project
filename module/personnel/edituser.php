0b66640ee7e0a5cc415a50f636df8e68af259941
one99 <goodloveone1@gmail.com>
2018-07-05 13:27:26 +0700

    update user

--- module/personnel/edituser.php
+++ module/personnel/edituser.php
@@ -36,6 +36,7 @@
 			<div class="form-group row">
 				<label for="staticEmail" class="col-sm-2 col-form-label">คำนำหน้า</label>
 				<div class="col-sm-10">
+				<input type="text" class="form-control" placeholder="id" name="gen_id" value="<?php echo $gen_id ?>" hidden>
 					<input type="text" class="form-control" id="staticEmail" placeholder="คำนำหน้า" name="titlename" value="<?php echo $gen_prefix ?>">
 				</div>
 			</div>
@@ -157,76 +158,22 @@
 					<input type="password" class="form-control"  placeholder="ConPW" name="conPW" value="<?php echo $gen_pass ?>">
 				</div>	
 			</div>
-			
+			<div class="form-group row">
+				<label for="" class="col-md-2 col-form-label">ประเภทบุคลากร</label>
+				<div class="col-md-10">
+					<select class="form-control"  name="permiss">
+						<?php
+							$permiss = mysqli_query($con,"SELECT  permiss_id,permiss_decs FROM permissions") or die ("error".mysqli_error($con));
+							while(list($permissid,$permissname) = mysqli_fetch_row($permiss)){
+								echo "<option value='".$permissid."'>$permissname</option>";
+							}
+							mysqli_free_result($permiss);
+						?>
+					</select>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-12"> 		<!-- >ปริญญาตรี -->
-			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
-				<div class="col-sm-12" >
-					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาตรี</label>
-				</div>
-				
-				<span class="col-sm-12">
-					<div class='row'>
-						
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary adddegree1 btn-block">เพิ่ม</button>
-						</div>
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary cleardegree1 btn-block">CLEAR</button>
-						</div>
-
-						<ul class="loaddegree col-12  list-group"></ul>
-						
-					</div>
-				</span>
-			</div>
-		</div> <!-- > END ปริญญาตรี -->
-
-		<div class="col-md-12"> 		<!-- >ปริญญาโท -->
-			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
-				<div class="col-sm-12" >
-					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาโท</label>
-				</div>
-				
-				<span class="col-sm-12" >
-					<div class='row'>
-						
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary adddegree2 btn-block">เพิ่ม</button>
-						</div>
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary cleardegree2 btn-block">CLEAR</button>
-						</div>
-
-						<ul class="loaddegree2 col-12  list-group"></ul>
-						
-					</div>
-				</span>
-			</div>
-		</div> <!-- > END ปริญญาโท -->
-
-		<div class="col-md-12"> 		<!-- >ปริญญาเอก -->
-			<div class="form-group row p-1 pb-2 m-1" style="border: solid 2px;border-radius: 25px;">
-				<div class="col-sm-12" >
-					<label for="staticEmail" class="col-sm-12 col-form-label">ปริญญาเอก</label>
-				</div>
-				
-				<span class="col-sm-12">
-					<div class='row'>
-						
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary adddegree3 btn-block">เพิ่ม</button>
-						</div>
-						<div class="col-md">
-							<button type="button" class="btn btn-secondary cleardegree3 btn-block">CLEAR</button>
-						</div>
-
-						<ul class="loaddegree3 col-12  list-group"></ul>
-						
-					</div>
-				</span>
-			</div>		
-		</div> <!-- > END ปริญญาโท -->
+		
 		<div class="col-md-12 text-center mb-2" >
 		<button type="button" class="btn updateuser" data-modules="personnel" data-action="updateuser"> บันทึก </button>
 		</div>
@@ -255,152 +202,7 @@
 				selectsuj();					
 			});
 
-		
-
-// ปริญญาตรี 
-
-			$("button.cleardegree1").on("click",function(e) {
-				e.preventDefault();
-			    var r = confirm("คณต้องการล้างใช่ไหม?");
-			    if(r == true){
-					$('.loaddegree').html("");
-				}
-			});
-
-			$('.loaddegree').on('click', '.deldegree1', function(e) {
-			    e.preventDefault();
-			    var r = confirm("คณต้องการลบใช่ไหม?");
-            	if (r == true) {
-			    	$(this).parent().remove();
-				}
-
-			});
-
-
-			$("button.adddegree1").on("click",function(e) {
-  				e.preventDefault();
-			    degreeload1()
-			});
-
-			function degreeload1(){
-
-				var count =$('.loaddegree li').length
-
-					if(count < 10 && count >= 0){
-						var text = "<li class='list-group-item'>"
-						+ "<input type='text' class='form-control'  placeholder='คำนำหน้า' name='a1[]'> "
-						+ "<input type='text'class='form-control'  placeholder='จบที่'  name='a2[]'> "
-						+ "<button type='button' class='btn btn-danger btn-block deldegree1 '>ลบ</button>"
-						+ "</li>"
-				
-						 $(".loaddegree").append(text);
-					}else{
-						alert("ไม่สามารเพื่มได้แล้ว");
-					}		
-
-					}
-
-// END ปริญญาตรี 
-
-//  ปริญญาโท
-
-			$("button.cleardegree2").on("click",function(e) {
-				e.preventDefault();
-			    var r = confirm("คณต้องการล้างใช่ไหม?");
-			    if(r == true){
-					$('.loaddegree2').html("");
-				}
-			});
-
-			$("button.adddegree2").on("click",function(e) {
-  				e.preventDefault();
-			    degreeload2()
-			});
-
-			$('.loaddegree2').on('click', '.deldegree2', function(e) {
-			    e.preventDefault();
-			    var r = confirm("คณต้องการลบใช่ไหม?");
-            	if (r == true) {
-			    	$(this).parent().remove();
-				}
-
-			});
-
-
-				function degreeload2(){
-
-					var count =$('.loaddegree2 li').length
-
-					if(count < 10 && count >= 0){
-						var text = "<li class='list-group-item'>"
-						+ "<input type='text' class='form-control'  placeholder='คำนำหน้า' name='a1[]'> "
-						+ "<input type='text'class='form-control'  placeholder='จบที่'  name='a2[]'> "
-						+ "<button type='button' class='btn btn-danger btn-block deldegree2 '>ลบ</button>"
-						+ "</li>"
-				
-						 $(".loaddegree2").append(text);
-					}else{
-						alert("ไม่สามารเพื่มได้แล้ว");
-					}		
-
-					}
-
-// END ปริญญาโท
-
-// ปริญญาเอก 
-
-			$("button.cleardegree3").on("click",function(e) {
-				e.preventDefault();
-			    var r = confirm("คณต้องการล้างใช่ไหม?");
-			    if(r == true){
-					$('.loaddegree3').html("");
-				}
-			});			
-
-			$("button.adddegree3").on("click",function(e) {
-  				e.preventDefault();
-			    degreeload3()
-			});
-
-			$('.loaddegree3').on('click', '.deldegree3', function(e) {
-			    e.preventDefault();
-			    var r = confirm("คณต้องการลบใช่ไหม?");
-            	if (r == true) {
-			    	$(this).parent().remove();
-				}
-
-			});
-
-
-				function degreeload3(){
-
-					var count =$('.loaddegree3 li').length
-
-					if(count < 10 && count >= 0){
-						var text = "<li class='list-group-item'>"
-						+ "<input type='text' class='form-control'  placeholder='คำนำหน้า' name='a1[]'> "
-						+ "<input type='text'class='form-control'  placeholder='จบที่'  name='a2[]'> "
-						+ "<button type='button' class='btn btn-danger btn-block deldegree3 '>ลบ</button>"
-						+ "</li>"
-				
-						 $(".loaddegree3").append(text);
-					}else{
-						alert("ไม่สามารเพื่มได้แล้ว");
-					}		
-
-					}
-
-// END ปริญญาเอก
-
 			
-
-			
-
-		
-
-
-
-					
 			$(".updateuser").click(function(){
 
 				 $.post( "module/personnel/updateuser.php", $( "#edituser" ).serialize()).done(function(data,txtstuta){
--- module/personnel/updateuser.php
+++ module/personnel/updateuser.php
@@ -1,7 +1,7 @@
 <?php
 include("../../function/db_function.php");
 $con=connect_db();
-
+echo $_POST['gen_id'],"/";
 echo $_POST['titlename'],"/";
 echo $_POST['name'],"/";
 echo $_POST['lname'],"/";
@@ -15,11 +15,14 @@ echo $_POST['gen_startdate'],"/";
 echo $_POST['level_id'],"/";
 echo $_POST['uname'],"/";
 echo $_POST['passwd'],"/";
+echo $_POST['permiss'],"/";
 
-$update="UPDATE general SET gen_id='',gen_user=' $_POST[uname]',gen_pass='$_POST[passwd]',branch_id='$_POST[brn]',subject_id='$_POST[suj]',gen_code=' $_POST[codeid]',gen_prefix=' $_POST[titlename]',gen_fnam='$_POST[name]',gen_lname='$_POST[lname]',
-gen_salary=' $_POST[salary]',gen_acadeic='$_POST[ap]',level_id='$_POST[level_id]',gen_startdate='',permiss_id='',gen_pos='',genpict='' ";
 
-echo $update;
+$update="UPDATE general SET gen_id='',gen_user=' $_POST[uname]',gen_pass='$_POST[passwd]',branch_id='$_POST[brn]',subject_id='$_POST[suj]',gen_code=' $_POST[codeid]',gen_prefix=' $_POST[titlename]',gen_fname='$_POST[name]',gen_lname='$_POST[lname]',
+gen_salary=' $_POST[salary]',gen_acadeic='$_POST[ap]',level_id='$_POST[level_id]',gen_startdate='$_POST[gen_startdate]',permiss_id='echo $_POST[permiss]',gen_pos='echo $_POST[pos]',gen_pict='' WHERE  gen_id='$_POST[gen_id]'";
+
+
+mysqli_query($con,$update)or die("SQL Error=>".mysqli_error($con));
 /*echo $_POST['titlename'];
 $a1 =  $_POST['a1'];
 
@@ -27,4 +30,6 @@ foreach ($a1 as $value) {
 	echo "$value => ";
 }
 */
+
+mysqli_close($con);
 ?>
