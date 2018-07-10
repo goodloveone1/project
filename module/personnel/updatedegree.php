d9a86d09c65705632ed7dbc3aa57a42a9064eb45
one99 <goodloveone1@gmail.com>
2018-07-10 12:51:57 +0700

    ed

--- module/personnel/updatedegree.php
+++ module/personnel/updatedegree.php
@@ -7,9 +7,9 @@ echo $ed_id = $_POST['ed_id'];
 echo $ed_name = $_POST['ed_name'];
 echo $ed_loc = $_POST['ed_loc'];
 echo"<hr>";
-//$sql = "UPDATE education SET ed_id = '$ed_id' , ed_name = '$ed_name',ed_loc = '$ed_loc',degree_id='$id_degree'  WHERE  ed_id = '$ed_id'";
+$sql = "UPDATE education SET ed_id = '$ed_id' , ed_name = '$ed_name',ed_loc = '$ed_loc',degree_id='$id_degree'  WHERE  ed_id = '$ed_id'";
 
-echo $sql;
+//echo $sql;
 $result=mysqli_query ($con,$sql) or die ("error".mysqli_error($con));
 
 ?>
