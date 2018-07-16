4aff5593e68f12ec3bd336b1fc3111964cf85711
beer1973 <pop197317@gmail.com>
2018-07-16 14:37:10 +0700

    add

--- module/personnel/loaddatadegree.php
+++ module/personnel/loaddatadegree.php
@@ -1,33 +1,24 @@
 <?php
 include("../../function/db_function.php");
 $con=connect_db();
-
 $gen_id = $_POST['getid'];
-
-
 $data=array();
 						$degree = mysqli_query($con,"SELECT  ed_id,degree_id,ed_name,ed_loc FROM education WHERE gen_id='$gen_id'") or die ("error".mysqli_error($con));
 						while(list($ed_id,$degree_id,$ed_name,$ed_loc)=mysqli_fetch_row($degree)){
 							$deName = mysqli_query($con,"SELECT degree_name FROM degree WHERE degree_id='$degree_id'")or die("errorSQL".mysqli_error($con));
 							list($degree_name)=mysqli_fetch_row($deName);
-
-		$addarry = array($degree_name,$ed_name,$ed_loc, "<a href='#'class='editbrn' data-iddegree='$ed_id' data-toggle='modal' ><i class='fas fa-edit fa-2x'></i></a>",
-					"<a href='#' class='delbrn' data-degreename='$degree_name' data-iddegree='$ed_id'><i class='fas fa-trash-alt fa-2x'></i></a>"
-						);
+							$addarry = array($degree_name,$ed_name,$ed_loc, "<a href='#'class='editbrn' data-iddegree='$ed_id' data-toggle='modal' ><i class='fas fa-edit fa-2x'></i></a>",
+										"<a href='#' class='delbrn' data-degreename='$degree_name' data-iddegree='$ed_id'><i class='fas fa-trash-alt fa-2x'></i></a>"
+											);
 				
-
-						array_push($data,$addarry);	
+							array_push($data,$addarry);
 						}
 						mysqli_free_result($degree);
-			$addarry = array("<button type='button' class='btn' id='adddegree' data-genid='$gen_id' data-toggle='modal'>เพิ่มวุฒิการศึกษา</button>","","","","");
-
-			array_push($data,$addarry);	
+			// $addarry = array("<button type='button' class='btn' id='adddegree' data-genid='$gen_id' data-toggle='modal'>เพิ่มวุฒิการศึกษา</button>","","","","");
+			// 	array_push($data,$addarry);
 	
 			
 			
-
-
-echo	json_encode($data);	
+		echo	json_encode($data);
 	
-
 ?>
