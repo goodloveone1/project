0645e663e9d809cbbcad249cdfd410b14bada821
one99 <goodloveone1@gmail.com>
2018-08-16 15:25:08 +0700

    fg

---
+++ module/assessment/test.php
@@ -0,0 +1,38 @@
+
+<html>
+
+<head>
+<title>opendir()</title>
+</head>
+
+<body>
+
+<?php
+
+
+
+    $dir = "../../img";
+    mkdir("../../img/gg");
+    // Open a directory, and read its contents
+    if (is_dir($dir)){
+      if ($dh = opendir($dir)){
+        while (($file = readdir($dh)) !== false){
+          echo "ไฟล์ :" . $file . "<br>";
+        }
+        closedir($dh);
+      }
+    }
+
+    if( file_exists("../../img/e810zihfvd_2344.jpg") )
+{
+echo "<p style='color:red;'>มีไฟล์แล้ว<p>";
+}
+else
+{ 
+echo "ไม่มีไฟล์";
+}
+    ?>
+
+
+</body>
+</html>
