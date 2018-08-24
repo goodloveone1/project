e2b62b2a37adb28128dde4a14ce06a7ea014dc1e
one99 <goodloveone1@gmail.com>
2018-08-24 14:02:22 +0700

    upddd

--- function/fc_time.php
+++ function/fc_time.php
@@ -1,6 +1,4 @@
 <?php
-
-
     function DateThai($strDate){
     $strYear = date("Y",strtotime($strDate))+543;
     $strMonth= date("n",strtotime($strDate));
@@ -9,8 +7,7 @@
     $strMonthThai=$strMonthCut[$strMonth];
     return "$strDay $strMonthThai $strYear";
     }
-
-
+    
     function DateThaiTime($strDate){
         $strYear = date("Y",strtotime($strDate))+543;
         $strMonth= date("n",strtotime($strDate));
@@ -23,6 +20,7 @@
         return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
         }
     
+        //คำนวณ วัน เดือน ปี
         function dateDifference($date_1 , $date_2 , $differenceFormat = '%a' ){
         $datetime1 = date_create($date_1);
         $datetime2 = date_create($date_2);
