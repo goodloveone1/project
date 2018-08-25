6f44f7aa44fc5ecaee9f8006a83fc0f8e3e744ba
one99 <goodloveone1@gmail.com>
2018-08-25 11:58:22 +0700

    up

--- function/fc_time.php
+++ function/fc_time.php
@@ -30,5 +30,4 @@
         return $interval->format($differenceFormat);
         
         }
-
 ?>
---
+++ module/assessment/ldl.php
@@ -0,0 +1,63 @@
+<form id="foreditbrc">
+ <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header headtitle">
+                    <h5 class="modal-title" id="exampleModalLabel"> บันทึกการมาปฏิบัติงาน</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label > ลาป่วย :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > ลากิจ :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > มาสาย :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > ลาคลอดบุตร :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > ลาอุปสมบท :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > ลาป่วยจำเป็นต้องรักษาตัวเป็นเวลานาน
+คราวเดียวหรือหลายคราวรวมกัน :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                    <div class="form-group">
+                        <label > ขาดราชการ :</label>
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+                    <button type="button" class="btn btn-primary" id="updatesu">บันทึก</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
