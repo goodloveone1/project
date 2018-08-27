da945f31bb93b1bda7ac64f7756bc59f384790cc
one99 <goodloveone1@gmail.com>
2018-08-27 12:04:12 +0700

    78

--- module/assessment/ldl.php
+++ module/assessment/ldl.php
@@ -1,4 +1,17 @@
-<form id="foreditbrc">
+<?php
+    include("../../function/db_function.php");
+	$con=connect_db();
+
+    $seldlt=mysqli_query($con,"SELECT *FROM idlel_type")or die(mysqli_error($con));
+    // while($resuat=mysqli_fetch_assoc($seldlt)){
+    //     print_r($resuat);
+    // }
+
+    for ($set = array (); $row = $seldlt->fetch_assoc(); $set[] = $row);
+print_r($set);
+?>
+
+<form id="foreditbrc" method="get" action="ldl_insert.php">
  <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -11,51 +24,52 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label > ลาป่วย :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                        <input type="text"   class="form-control" value=""  name="i_no" size=10 hiddent >
+                       <input type="text"   class="form-control" value=""  name="i_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="i_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > ลากิจ :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="g_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="g_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > มาสาย :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="l_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="l_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > ลาคลอดบุตร :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="s_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="s_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > ลาอุปสมบท :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="b_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="b_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > ลาป่วยจำเป็นต้องรักษาตัวเป็นเวลานาน
 คราวเดียวหรือหลายคราวรวมกัน :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="sex_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="sex_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                     <div class="form-group">
                         <label > ขาดราชการ :</label>
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="branch_name" size=10 > วัน
+                       <input type="text"   class="form-control" value=""  name="p_no" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="p_day" size=10 > วัน
                        <input type="hidden"    value=""  name="branch_id" size=40 require>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
-                    <button type="button" class="btn btn-primary" id="updatesu">บันทึก</button>
+                    <button type="submit" class="btn btn-primary" id="updatesu">บันทึก</button>
                 </div>
             </div>
         </div>
---
+++ module/assessment/ldl_insert.php
@@ -0,0 +1,8 @@
+<?php
+	include("../../function/db_function.php");
+	$con=connect_db();
+
+   echo $_GET['i_no'];
+    
+
+?>
