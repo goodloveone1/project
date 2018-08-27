bf8a7533a892851cf468d879c05675cd8dc9bea9
one99 <goodloveone1@gmail.com>
2018-08-27 13:26:51 +0700

    up

--- module/assessment/ldl.php
+++ module/assessment/ldl.php
@@ -3,13 +3,10 @@
 	$con=connect_db();
 
     $seldlt=mysqli_query($con,"SELECT *FROM idlel_type")or die(mysqli_error($con));
-    // while($resuat=mysqli_fetch_assoc($seldlt)){
-    //     print_r($resuat);
-    // }
-
     for ($set = array (); $row = $seldlt->fetch_assoc(); $set[] = $row);
-print_r($set);
-echo "<br>";
+    //print_r($set);
+
+    echo $set[0]['idl_type_name'];
 ?>
 
 <form id="foreditbrc" method="get" action="ldl_insert.php">
@@ -23,50 +20,18 @@ echo "<br>";
                     </button>
                 </div>
                 <div class="modal-body">
+                <?php  for($i=0;$i<count($set);$i++){
+                    $name="value";
+                    $n=1 ?>
                     <div class="form-group">
-                        <label > ลาป่วย :</label>
-                        <input type="text"   class="form-control" value=""  name="i_no" size=10 hiddent >
-                       <input type="text"   class="form-control" value=""  name="i_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="i_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
+    
+                        <label > <?php echo $set[$i]['idl_type_name']; ?> :</label>
+                        <!-- <input type="text"   class="form-control" value=""  name="i_no" size=10 > -->
+                       <input type="text"   class="form-control" value=""  name="i_no<?php echo $i+1 ?>" size=10 > ครั้ง
+                       <input type="text"   class="form-control" value=""  name="i_day<?php echo $i+1 ?>" size=10 > วัน
+                       <input type="hidden"    value="<?php echo $set[$i]['idl_type_id']; ?>"  name="<?php  ?>" size=40 require>
                     </div>
-                    <div class="form-group">
-                        <label > ลากิจ :</label>
-                       <input type="text"   class="form-control" value=""  name="g_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="g_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
-                    <div class="form-group">
-                        <label > มาสาย :</label>
-                       <input type="text"   class="form-control" value=""  name="l_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="l_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
-                    <div class="form-group">
-                        <label > ลาคลอดบุตร :</label>
-                       <input type="text"   class="form-control" value=""  name="s_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="s_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
-                    <div class="form-group">
-                        <label > ลาอุปสมบท :</label>
-                       <input type="text"   class="form-control" value=""  name="b_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="b_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
-                    <div class="form-group">
-                        <label > ลาป่วยจำเป็นต้องรักษาตัวเป็นเวลานาน
-คราวเดียวหรือหลายคราวรวมกัน :</label>
-                       <input type="text"   class="form-control" value=""  name="sex_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="sex_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
-                    <div class="form-group">
-                        <label > ขาดราชการ :</label>
-                       <input type="text"   class="form-control" value=""  name="p_no" size=10 > ครั้ง
-                       <input type="text"   class="form-control" value=""  name="p_day" size=10 > วัน
-                       <input type="hidden"    value=""  name="branch_id" size=40 require>
-                    </div>
+                <?php   }?>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
