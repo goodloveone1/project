e0ebcc1bbb857a9679c6eaea549e3d7487bba634
beer1973 <pop197317@gmail.com>
2019-01-13 14:49:11 +0700

    add

--- function/menu.php
+++ function/menu.php
@@ -57,7 +57,7 @@ function webmenu($id){
 
 		case "2": // อาจาทย์
 ?>
-		<p> <i class="fas fa-user-circle fa-2x"> ยินดีต้อนรับ อาจารย์</p>
+		<p> <i class="fas fa-user-circle fa-2x"></i> ยินดีต้อนรับ อาจารย์</p>
 			<ul class="list-group" id="menuaside">
 				<a href="javascript:void(0)" class="menuuser " data-modules="personnel" data-action="home" >
 					<button class="btn-block bt-color"  >
@@ -120,7 +120,7 @@ function webmenu($id){
 
 		case 3: /// หัวหน้าหลักสูตร MENU
 ?>
-				<p> <i class="fas fa-user-circle fa-2x"> ยินดีต้อนรับ หัวหน้าหลักสูตร</p>
+				<p> <i class="fas fa-user-circle fa-2x"></i> ยินดีต้อนรับ หัวหน้าหลักสูตร</p>
 			<ul class="list-group" >
 				<a href="javascript:void(0)" class="menuuser " data-modules="staff" data-action="1" >
 					<button class="btn-block bt-color"  >
@@ -188,6 +188,140 @@ function webmenu($id){
 			</ul>
 <?php
 			break;
+			case "4": // อาจาทย์
+	?>
+			<p> <i class="fas fa-user-circle fa-2x"> ยินดีต้อนรับ อาจารย์</p>
+				<ul class="list-group" id="menuaside">
+					<a href="javascript:void(0)" class="menuuser " data-modules="personnel" data-action="home" >
+						<button class="btn-block bt-color"  >
+						<li class="list-group-item list-menu-user" >
+							<i class="icon fas fa-home fa-3x " ></i><span class="text"> หน้าหลัก</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="personnel" data-action="edituserall">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-user-cog fa-3x " ></i><span class="text">แก้ไขข้อมูลส่วนตัว</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-book-open fa-3x " ></i><span class="text">จัดการภาระงานของตน</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-archive fa-3x " ></i><span class="text">จัดการไฟล์หลักฐาน</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							&nbsp;<i class="icon fas fa-info fa-3x " ></i><span class="text"> &nbsp;รับทราบข้อตกลง TOR</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="menuassm">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-check-square fa-3x " ></i><span class="text"> ประเมินตนเอง</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-calendar-check fa-3x " ></i><span class="text">  ผลการประเมิน</span>
+						</li>
+						</button>
+					</a>
+				</ul>
+	<?php
+				break;
+
+			case 5: /// หัวหน้าหลักสูตร MENU
+	?>
+					<p> <i class="fas fa-user-circle fa-2x"> ยินดีต้อนรับ หัวหน้าหลักสูตร</p>
+				<ul class="list-group" >
+					<a href="javascript:void(0)" class="menuuser " data-modules="staff" data-action="1" >
+						<button class="btn-block bt-color"  >
+						<li class="list-group-item list-menu-user" >
+							<i class="icon fas fa-home fa-3x " ></i><span class="text">&nbsp;หน้าหลัก</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-user-cog fa-3x " ></i><span class="text">&nbsp;แก้ไขข้อมูลส่วนตัว</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-book-open fa-3x " ></i><span class="text">&nbsp;จัดการภาระงานของตน</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-archive fa-3x " ></i><span class="text">&nbsp;จัดการไฟล์หลักฐาน</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							&nbsp;<i class="icon fas fa-info fa-3x " ></i><span class="text">&nbsp;รับทราบข้อตกลง TOR</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-check-square fa-3x " ></i><span class="text">&nbsp;ประเมินตนเอง</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-user-check fa-3x " ></i><span class="text">&nbsp;ประเมินการบุคลากร</span>
+						</li>
+						</button>
+					</a>
+
+					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
+						<button class="btn-block bt-color">
+						<li class="list-group-item list-menu-user">
+							<i class="icon fas fa-calendar-check fa-3x " ></i><span class="text">&nbsp;ผลการประเมิน</span>
+						</li>
+						</button>
+					</a>
+				</ul>
+	<?php
+
+					break;
 
 			default: ERROE;
 
