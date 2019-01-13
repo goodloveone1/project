11dde71b7df8b0278d812e58ecde871d858f21d3
goodloveone1 <40379480+goodloveone1@users.noreply.github.com>
2019-01-13 19:44:48 +0700

    up

--- module/assessment/adddata_tor2.php
+++ module/assessment/adddata_tor2.php
@@ -9,22 +9,33 @@ $con=connect_db();
 // echo $tit_sub;
 $exp = $_POST['exp'];
 $scr = $_POST['go'];
+$Pscore = $_POST['score'];
 $st="stit";
+$x="x";
+$sg="sumgo";
 for($i=0;$i<14;$i++){
     $tit_sub=$st.$i;
     // echo $_POST[$tit_sub],"-";
     // echo $exp[$i],"-";
     $sql="INSERT INTO tort2(tort2_id,tor_id,tort2_sub_id,tort2_goal,tort2_score) 
     VALUES('','$_POST[tor_id]','$_POST[$tit_sub]','$exp[$i]','$scr[$i]')";
-   echo $sql;
-   mysqli_query ($con,$sql) or die ("error2".mysqli_error($con));
+  // echo $sql;
+  mysqli_query ($con,$sql) or die ("error1".mysqli_error($con));
 }
+for($i=0;$i<4;$i++){
+   
+    $px=$x.($i+1);
+    $sumgo=$sg.($i+1);
+    // echo "|",$_POST[$px],"-",$_POST[$sumgo],"-";
+    // echo $Pscore[$i];
+
+    $sql2="INSERT INTO tort2_skil(tort2s_id,tort_id,tort2s_skil,tort2s_x,tort2s_score)
+    VALUES('','$_POST[tor_id]','$_POST[$sumgo]','$_POST[$px]','$Pscore[$i]')";
+    //echo $sql2;
+    mysqli_query ($con,$sql2) or die ("error1".mysqli_error($con));
+}
+echo"บันทึกเสร็จแล้ว";
 mysqli_close($con);
-
-
-
-
-
 ?>
 
 
