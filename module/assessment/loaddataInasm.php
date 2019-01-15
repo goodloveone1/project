556d35c44be29af87d9831270c397c76692ce5d7
beer1973 <pop197317@gmail.com>
2019-01-15 20:11:22 +0700

    add

--- module/assessment/loaddataInasm.php
+++ module/assessment/loaddataInasm.php
@@ -12,16 +12,17 @@ $year = $_POST['year'];
     <tr>
       <th>ลำดับ </th>
       <th> รูปภาพ </th>
-      <th> รหัสบุดลากร </th>
       <th> ชื่อ </th>
       <th> สกุล </th>
+      <th> สาขา </th>
+      <th> หลักสูตร </th>
       <th> สถานะการประเมิน </th>
       <th> ประเมินบุคลากร </th>
     </tr>
   <thead>
     <?php
     if($_SESSION['user_level'] == 3){ // สาขา
-      $show= mysqli_query($con,"SELECT gen_id,gen_fname,gen_lname,branch_id,subject_id,gen_pict FROM general WHERE branch_id='$_SESSION[branch_id]' ") or  die("SQL Error1==>1".mysql_error($con));
+      $show= mysqli_query($con,"SELECT gen_id,gen_fname,gen_lname,branch_id,subject_id,gen_pict FROM general as WHERE branch_id='$_SESSION[branch_id]' ") or  die("SQL Error1==>1".mysql_error($con));
     }
     else if($_SESSION['user_level'] == 4){ // สาขา หลักสูตร
       $show= mysqli_query($con,"SELECT gen_id,gen_fname,gen_lname,branch_id,subject_id,gen_pict FROM general WHERE subject_id='$_SESSION[subject_id]'  ") or  die("SQL Error1==>1".mysql_error($con));
@@ -39,6 +40,14 @@ while(list($gen_id,$gen_fname,$gen_lname,$branch_id,$subject_id,$gen_pict)=mysql
     echo " <td>$gen_id</td>";
     echo " <td>$gen_fname</td>";
     echo " <td>$gen_lname</td>";
+
+    $ba= mysqli_query($con,"SELECT branch_name FROM branch WHERE branch_id='$branch_id'  ") or  die("SQL Error1==>1".mysql_error($con));
+    list($branch_name)=mysqli_fetch_row($ba);
+    $sb= mysqli_query($con,"SELECT subject_name FROM subjects WHERE subject_id='$subject_id'  ") or  die("SQL Error1==>1".mysql_error($con));
+    list($subject_id)=mysqli_fetch_row($sb);
+    echo " <td>$subject_id</td>";
+    echo " <td>$branch_name</td>";
+
     $show2= mysqli_query($con,"SELECT tor_id FROM tor WHERE gen_id='$gen_id' AND tor_year='$year'") or  die("SQL Error1==>1".mysql_error($con));
     list($tor_id)=mysqli_fetch_row($show2);
     if(!empty($tor_id)){
