ce78ba79014a7b3dd08c46328e0063e2ffc442d2
beer1973 <pop197317@gmail.com>
2019-01-16 14:00:34 +0700

    add

--- function/menu.php
+++ function/menu.php
@@ -244,62 +244,65 @@ function webmenu($id){
 	?>
 
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-user-cog fa-3x " ></i><span class="text">&nbsp;แก้ไขข้อมูลส่วนตัว</span>
-						</li>
-						</button>
-					</a>
+	<ul class="list-group" >
+		<a href="javascript:void(0)" class="menuuser " data-modules="personnel" data-action="home" >
+			<button class="btn-block bt-color"  >
+			<li class="list-group-item list-menu-user" >
+				<i class="icon fas fa-home fa-3x " ></i><span class="text">&nbsp;หน้าหลัก</span>
+			</li>
+			</button>
+		</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-book-open fa-3x " ></i><span class="text">&nbsp;จัดการภาระงานของตน</span>
-						</li>
-						</button>
-					</a>
+		<a href="javascript:void(0)" class="menuuser" data-modules="personnel" data-action="edituserall">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-user-cog fa-3x " ></i><span class="text">&nbsp;แก้ไขข้อมูลส่วนตัว</span>
+			</li>
+			</button>
+		</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-archive fa-3x " ></i><span class="text">&nbsp;จัดการไฟล์หลักฐาน</span>
-						</li>
-						</button>
-					</a>
+		<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="manage_record">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-book-open fa-3x " ></i><span class="text">บันทึกการปฏิบัติงาน</span>
+			</li>
+			</button>
+		</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							&nbsp;<i class="icon fas fa-info fa-3x " ></i><span class="text">&nbsp;รับทราบข้อตกลง TOR</span>
-						</li>
-						</button>
-					</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-check-square fa-3x " ></i><span class="text">&nbsp;ประเมินตนเอง</span>
-						</li>
-						</button>
-					</a>
+		<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="manage_asmIn">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-user-check fa-3x " ></i><span class="text">&nbsp;ประเมินบุคลากร</span>
+			</li>
+			</button>
+		</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-user-check fa-3x " ></i><span class="text">&nbsp;ประเมินการบุคลากร</span>
-						</li>
-						</button>
-					</a>
 
-					<a href="javascript:void(0)" class="menuuser" data-modules="staff" data-action="2">
-						<button class="btn-block bt-color">
-						<li class="list-group-item list-menu-user">
-							<i class="icon fas fa-calendar-check fa-3x " ></i><span class="text">&nbsp;ผลการประเมิน</span>
-						</li>
-						</button>
-					</a>
-				</ul>
+		<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="menuassm">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-check-square fa-3x " ></i><span class="text">&nbsp;ประเมินตนเอง</span>
+			</li>
+			</button>
+		</a>
+
+		<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="manage_Evidence">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-archive fa-3x " ></i><span class="text">&nbsp;จัดการไฟล์หลักฐาน</span>
+			</li>
+			</button>
+		</a>
+
+		<a href="javascript:void(0)" class="menuuser" data-modules="assessment" data-action="sum_assessment">
+			<button class="btn-block bt-color">
+			<li class="list-group-item list-menu-user">
+				<i class="icon fas fa-calendar-check fa-3x " ></i><span class="text">&nbsp;ผลการประเมิน</span>
+			</li>
+			</button>
+		</a>
+	</ul>
 	<?php
 
 					break;
