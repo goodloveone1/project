fb186e8b4ae6490aa29ee42549091abb3180de49
pop1973 <drive_1973@hotmail.com>
2019-02-19 17:16:12 +0700

    add

--- module/assessment/updateCriteria.php
+++ module/assessment/updateCriteria.php
@@ -4,7 +4,7 @@
     $con=connect_db();
 
     $update="UPDATE aptitudes SET  score= '$_POST[score]' WHERE atb_id= '$_POST[atbid]'"; 
-    echo $update;
+    //echo $update;
     mysqli_query($con,$update) or  die ("mysql error=>>".mysqli_error($con));
   
     mysqli_close($con);
