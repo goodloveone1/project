483afcedad1e6c90ca843890c69f0f87f675acfc
goodloveone1 <40379480+goodloveone1@users.noreply.github.com>
2019-03-19 23:42:23 +0700

    update

--- module/assessment/load_sum_ass.php
+++ module/assessment/load_sum_ass.php
@@ -172,7 +172,7 @@ if(!empty($inform==1)){
             mysqli_free_result($se_All);
       ?>
     </P>
-    <p><b>สรุปการประเมินผลการปฏิบัติราชการ</b></p>
+    <p><b>ส่วนที่ ๓ สรุปการประเมินผลการปฏิบัติราชการ </b></p>
 		<table class="table table-bordered text-center sa">
 			<tr >
 				<th>องค์ประกอบการประเมิน</th>
@@ -231,8 +231,314 @@ if(!empty($inform==1)){
    </div>
 </div>
 
+<div class="row">
+  <div class="col-md">
+  <p><b>ส่วนที่ ๔ แผนพัฒนาการปฏิบัติราชการรายบุคคล</b></p>
+  <table class="table table-bordered">
+				<tr>
+					<th>ความรู้/ทักษะ/สมรรถนะ ที่ต้องได้รับการพัฒนา </th>
+					<th>วิธีการพัฒนา</th>
+					<th>ช่วงเวลาที่ต้องการพัฒนา</th>
+				</tr>
+			
+					<?php $se_Asst4 = mysqli_query($con,
+						 "SELECT knowledge,develop,longtime FROM asessment_t4 WHERE  ass_id='$ass_id'")or die("SQL-error.asst4".mysqli_error($con)); 
+						 while(list($knowledge,$develop,$longtime)=mysqli_fetch_row($se_Asst4)){
+						 ?>
+					<tr>
+					<td><?php echo $knowledge ?></td>
+					<td><?php echo $develop?></td>
+					<td><?php echo $longtime ?></td>
+				</tr>
+						 <?php }?>
+			</table>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md">
+  <p><b>ส่วนที่ ๕ แจ้งผลการประเมิน</b></p>
+  <p>
+      <?php
+     
+        $select_tor=mysqli_query($con,"SELECT leader FROM assessments WHERE ass_id='$ass_id'") or die("SQL-error.SelectTor".mysqli_error($con));
+    list($hleader)=mysqli_fetch_row($select_tor);
+        //echo $hleader;
+    mysqli_free_result($select_tor);
+	$sql="SELECT  prefix,lname,fname,position FROM staffs WHERE st_id ='$_SESSION[user_id]'";
+	$genchk= mysqli_query($con,$sql) or die ("gen_chk".mysqli_error($con));
+	list($tle_g,$g_lname,$g_fname,$g_pos)=mysqli_fetch_row($genchk);
+	mysqli_free_result($genchk);
+  
+
+    $seAss5 =mysqli_query($con,
+    "SELECT asst5_id,accept,inform,date_accept,date_inform
+    FROM asessment_t5
+    WHERE ass_id='$ass_id' " )or die("SQL-error.asAss5".mysqli_error($con));
+    list($asst5_id,$accept,$inform,$date_accept,$date_inform)=mysqli_fetch_row($seAss5);
+		
+        if($inform==1){
+					$chk_inform = "checked";
+					$ac="";
+        }else{
+					$chk_inform = "";
+					$ac="ac";
+        }
+        if($accept==1){
+          $chk_accept = "checked";
+        }else{
+          $chk_accept = "";
+        }
+        $date = date("Y/m/d");
+      ?>
+  </p>
+  <div class="row">
+	<div class="col-md-6 border border-dark p-3">
+		<p>ผู้รับการประเมิน :</p>
+		<div class="custom-control custom-checkbox">
+			  <input class="custom-control-input" type="checkbox" value="1" id="ac" name="ac"  <?php echo $chk_accept?>>
+			  <label class="custom-control-label" for="ac">
+			    รับทราบผลการประเมินและแผนพัฒนา การปฏิบัติราชการรายบุคคลแล้ว
+
+			  </label>
+		</div>
+
+	</div>
+	<div class="col-md-6 border   border-dark p-3">
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ชื่อ</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $tle_g,$g_fname,"  ",$g_lname; ?>" name="uname" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ตำแหน่ง</label>
+				<?php     
+					$sqlspos ="SELECT pos_name FROM position WHERE pos_id='$g_pos'";
+					$sespos=mysqli_query($con,$sqlspos) or die("sePos".mysqli_error($con));
+					list($sname_pos)=mysqli_fetch_row($sespos);	
+					mysqli_free_result($sespos);
+				?>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $sname_pos?>" name="upos" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">วันที่</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php if($date_accept=="0000-00-00"){echo "";}else{echo DateThai($date_accept); }  ?>"  name="udate" readonly >
+					<input type="hidden" name="usdate" value="">
+				</div>				
+		</div>
+	</div>
+	<!-- ผู้ประเมิน : -->
+	<div class="col-md-6 border border-dark p-3">
+	<p>ผู้ประเมิน :</p>
+		<div class="custom-control custom-checkbox">
+			  <input class="custom-control-input" type="checkbox" vlue="1" name="tappcetp" id="customCheck1" <?php echo $chk_inform?> readonly>
+			  <label class="custom-control-label" for="" >
+			   แจ้งผลการประเมิน
+			  </label>
+		</div>
+		<!-- <div class="form-check">
+			  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+			  <label class="form-check-label" for="defaultCheck1">
+			   ได้แจ้งผลการประเมินเมื่อวันที่.............................................
+      			แต่ผู้รับการประเมินไม่ลงนามรับทราบผลการ
+     			ประเมินโดยมี………………..........เป็นพยาน
+			  </label>
+		</div> -->
 
 
+	</div>
+	<div class="col-md-6 border   border-dark p-3">
+		<div class="form-group row">
+		<?php  
+				$sql="SELECT  prefix,lname,fname,position FROM staffs WHERE st_id ='$hleader'";
+				$Lchk= mysqli_query($con,$sql) or die ("gen_chk".mysqli_error($con));
+				list($Lprefix,$Llname,$Lfname,$Lposition)=mysqli_fetch_row($Lchk);
+			
+				mysqli_free_result($Lchk);
+		?>
+				<label  class="col-sm-2 col-form-label">ชื่อ</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $Lprefix,$Lfname," ",$Llname ?>" name="sname" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ตำแหน่ง</label>
+				<?php     
+					$sqlLpos ="SELECT pos_name FROM position WHERE pos_id='$Lposition'";
+					$sesLpos=mysqli_query($con,$sqlLpos) or die("sePos".mysqli_error($con));
+					list($Lname_pos)=mysqli_fetch_row($sesLpos);	
+					mysqli_free_result($sesLpos);
+				?>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $Lname_pos;  ?>" name="t_pos" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">วันที่</label>
+				<div class="col-sm">
+			
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo DateThai($date)?>" name="" readonly>
+					<input type="hidden" value="<?php echo $date ?>" name="tdate">
+				</div>				
+		 </div>
+	 </div>
+
+   </div>
+  </div>
+</div>
+
+<div class="row">
+	 <div class="col-md">
+				<br>
+				<p><b>ส่วนที่ ๖ ความเห็นของผู้บังคับบัญชาเหนือขึ้นไป</b></p>
+				<p>
+					<?php   
+							$sqlyesr="SELECT ass_id,hleader,sleader FROM assessments WHERE  ass_id='$ass_id'";
+							$reChk = mysqli_query($con,"$sqlyesr") or die("torChk".mysqli_error($con));
+							list($tor_ID,$hightL,$supterL)=mysqli_fetch_row($reChk);
+							mysqli_free_result($reChk);
+
+							$sqlA6="SELECT leader_comt,leader_comt_disc,leader_compt_date,supervisor_comt,supervisor_comtdisc,supervisor_comt_date FROM asessment_t6 WHERE  ass_id='$ass_id'";
+							$seAss6 = mysqli_query($con,"$sqlA6") or die("seAss6".mysqli_error($con));
+							list($leader_comt,$leader_comt_disc,$leader_compt_date,$supervisor_comt,$supervisor_comtdisc,$supervisor_comt_date)=mysqli_fetch_row($seAss6);
+							mysqli_free_result($seAss6);
+						//echo $leader_comt,">>",$leader_comt_disc,"<<<",$leader_compt_date,$supervisor_comt,$supervisor_comtdisc,$supervisor_comt_date;
+							if($leader_comt==1){
+									$apc0="";
+									$apc1="checked";
+							}else if($leader_comt==0){
+								$apc0="";
+								$apc1="";
+							}else{
+								$apc0="checked";
+								$apc1="";
+							}
+
+							
+							if($supervisor_comt==1){
+								$uagree0="";
+								$uagree1="checked";
+						}else if($supervisor_comt==0){
+							$uagree0="";
+							$uagree1="";
+						}else{
+							$uagree0="checked";
+							$uagree1="";
+						}
+					?>
+				</p>
+	 </div>
+</div>
+<div class=row>
+<div class="col-md-6 border border-dark p-3">
+		<p>ผู้บังคับบัญชาเหนือขึ้นไป</p>
+		<div class="custom-control custom-radio">
+			  <input class="custom-control-input" type="radio" value="0" id="customRadio1" name="apc" <?php echo $apc0; ?> disabled  >
+			  <label class="custom-control-label" for="customRadio1">
+			    เห็นด้วยผลการประเมิน
+
+			  </label>
+		</div>
+		<div class="custom-control custom-radio">
+			  <input class="custom-control-input" type="radio" value="1" id="customRadio2" name="apc" <?php echo $apc1; ?>  disabled>
+			  <label class="custom-control-label" for="customRadio2">
+			    มีความเห็นแตกต่าง  ดังนี้
+
+			  </label>
+		</div>
+		<div class="form-group">
+		    <textarea class="form-control" name="hcompt" id="text1" rows="3"  disabled required><?php echo $leader_comt_disc ?></textarea>
+		 </div>
+
+	</div>
+	<div class="col-md-6 border   border-dark p-3">
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ลงชื่อ</label>
+			
+				<div class="col-sm">
+				<?php
+					 $sehleader=mysqli_query($con,"SELECT prefix,fname,lname,position FROM staffs WHERE st_id='$hightL'")or die("SQL.hleaderError".mysqli_error($con));
+					 list($hl_prefix,$hl_name,$hl_fname,$hl_position)=mysqli_fetch_row($sehleader);
+					 mysqli_free_result($sehleader);
+				?>
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $hl_prefix,$hl_name," ",$hl_fname ?>" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ตำแหน่ง</label>
+				<div class="col-sm">
+				<?php
+						$se_hlpostion = mysqli_query($con,"SELECT pos_name FROM position WHERE pos_id ='$hl_position'") or die("SQL.posHL_ERRor".mysqli_error($con));
+						list($hl_posName)=mysqli_fetch_row($se_hlpostion);
+
+				?>
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $hl_posName  ?>" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">วันที่</label>
+				<div class="col-sm">
+				<?php $date= date("Y/m/d")  ?>
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo DateThai($leader_compt_date)  ?> " readonly >
+				</div>				
+		</div>
+	</div>
+
+	<div class="col-md-6 border border-dark p-3">
+               <?php
+                     $seSleader=mysqli_query($con,
+                     "SELECT staffs.prefix,staffs.fname,staffs.lname,position.pos_name
+                     FROM staffs
+                     INNER JOIN position
+                     ON staffs.position=position.pos_id
+                     WHERE st_id='$supterL'")or die("SQL.hleaderError".mysqli_error($con));
+					 list($Sl_prefix,$Sl_name,$Sl_fname,$Sl_position)=mysqli_fetch_row($seSleader);
+					 mysqli_free_result($seSleader);
+				?>
+
+		<p>ผู้บังคับบัญชาเหนือขึ้นไปอีกชั้นหนึ่ง  (ถ้ามี)</p>
+		<div class="custom-control custom-radio">
+			  <input class="custom-control-input" type="radio" value="0" id="customRadio3" name="uagree" <?php echo $uagree0  ?> disabled >
+			  <label class="custom-control-label" for="customRadio3">
+			    เห็นด้วยผลการประเมิน
+
+			  </label>
+		</div>
+		<div class="custom-control custom-radio">
+			  <input class="custom-control-input" type="radio" value="1" id="customRadio4" name="uagree"<?php echo $uagree1  ?> disabled>
+			  <label class="custom-control-label" for="customRadio4">
+			    มีความเห็นแตกต่าง  ดังนี้
+			  </label>
+		</div>
+		<div class="form-group">
+		    <textarea class="form-control" name="scompt" id="text2" rows="3" disabled required><?php echo $supervisor_comtdisc ?></textarea>
+		 </div>
+	</div>
+	<div class="col-md-6 border   border-dark p-3">
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ลงชื่อ</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $Sl_prefix,$Sl_name," ",$Sl_fname ?>" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">ตำแหน่ง</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo $Sl_position  ?>" readonly>
+				</div>				
+		</div>
+		<div class="form-group row">
+				<label  class="col-sm-2 col-form-label">วันที่</label>
+				<div class="col-sm">
+					<input type="text" class="form-control" id="inputEmail3" placeholder="" value="<?php echo DateThai ($supervisor_comt_date) ?>" readonly>
+				</div>				
+		</div>
+	</div>
+</div>
 
 <?php 
 }else{
@@ -241,5 +547,23 @@ if(!empty($inform==1)){
 
 mysqli_close($con);
 
-
 ?>
+
+<script>
+	$('#ac').click(function() {
+				
+					if($(this).prop("checked") == true){
+						$.post( "module/assessment/update_ac.php", {tor_id: "<?php echo $asst5_id ?>", ac: "1"}).done(function( data ){
+									//alert(data);
+						var d = "<?php $date ?>";
+							
+  						});
+            }
+            else if($(this).prop("checked") == false){
+							$.post( "module/assessment/update_ac.php", {tor_id: "<?php echo $asst5_id ?>", ac: ""}).done(function( data ){
+									//alert(data);
+  								});
+            }
+			});
+
+</script>
