6a4537ff8be1292d378cefe351c1066e55522cd8
beer1973 <pop197317@gmail.com>
2019-04-24 16:22:54 +0700

    debug

--- js/functionjs.js
+++ js/functionjs.js
@@ -10,9 +10,7 @@ function loadmain(module1,action){
 	sessionStorage.setItem("module1", module1);
 	sessionStorage.setItem("action", action);
 
-
-
-	$.getScript("js/module.js", function() {
+	$.getScript("js/module.js");
 		var url = modules(module1 ,action);
 
 		$.post( url, { name: "John"})
@@ -24,7 +22,7 @@ function loadmain(module1,action){
 	  });
 			  
 
-	});
+
 }
 
 function loadingpage(module1,action){
