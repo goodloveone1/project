1fc66a2992e70a2fdcc3e13f0a0f9315e1e56f06
goodloveone1 <40379480+goodloveone1@users.noreply.github.com>
2019-05-07 22:02:32 +0700

    Update del_ass.php

--- module/assessment/del_ass.php
+++ module/assessment/del_ass.php
@@ -4,8 +4,22 @@ $con=connect_db();
 $sql="DELETE FROM assessments WHERE ass_id = '$_POST[id]'";
 //echo $sql;
 
-mysqli_query($con,$sql)or die("SQL.error".mysqli_error($con));
 
-echo "ลบ $_POST[type] ของ $_POST[user]เสร็จแล้ว";
+
+if($_POST['type']=="ประเมิน"){
+   $ass_id = $_POST['id'];
+   $e=substr($ass_id,3,11);
+   $evd_id="EVD".$e;
+   $se_file=mysqli_query($con,"SELECT evd_file_name FROM evidence_file WHERE evd_id='$evd_id'")or die("SQL.error".mysqli_error($con));
+    while(list($evd_name)=mysqli_fetch_row($se_file)){
+       // echo $evd_name;
+        unlink("../../file/$ass_id/$evd_name");
+    }
+    mysqli_free_result($se_file);
+
+    echo "ลบ $_POST[type] ของ $_POST[user]เสร็จแล้ว";
+    rmdir("../../file/$ass_id");
+}
+mysqli_query($con,$sql)or die("SQL.error".mysqli_error($con));
 mysqli_close($con);
 ?>
